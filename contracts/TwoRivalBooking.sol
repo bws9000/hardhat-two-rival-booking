d63771d012d8b19ffa191acf45de3cf423035fcf
bws9000 <snyder.burt@gmail.com>
2022-01-04 00:11:19 -0500

    update

--- contracts/TwoRivalBooking.sol
+++ contracts/TwoRivalBooking.sol
@@ -8,16 +8,16 @@ contract TwoRivalBooking {
     event RegistrationCleared(bool status);
 
     address facilitator;
+    address[] ActiveUsers;
     
     string groupA;
     string groupB;
-    uint8 groupSize;
+    uint256 totalRegistered;
+    uint256 totalBooked;
+    uint8 roomLimit;
 
-    uint256 public totalRegistered;
-    mapping(address => mapping(uint8 => uint8[24] )) groupABooking;
-    mapping(address => mapping(uint8 => uint8[24])) groupBBooking;
-    
-    address[] ActiveUsers;
+    mapping(uint8 => mapping(address => uint8[24])) groupABooking;
+    mapping(uint8 => mapping(address => uint8[24])) groupBBooking;
     
     mapping(address => bool) registeredUsers;
     mapping(address => string) userToGroup;
@@ -26,6 +26,7 @@ contract TwoRivalBooking {
         facilitator = msg.sender;
         groupA = "P";
         groupB = "C";
+        roomLimit = 10;
     }
 
     function registerUser(string calldata _group) external {
@@ -36,6 +37,17 @@ contract TwoRivalBooking {
         keccak256(bytes(_group)) != keccak256(bytes(groupB))){
             revert("group does not exist");
         }
+
+        bool unique = true;
+        for(uint8 i=0;i<ActiveUsers.length;i++){
+            if(ActiveUsers[i] == msg.sender){
+                unique = false;
+            }
+        }
+        if(unique){
+            ActiveUsers.push(msg.sender);
+        }
+
         registeredUsers[msg.sender] = true;
         userToGroup[msg.sender] = _group;
         ++totalRegistered;
@@ -49,59 +61,68 @@ contract TwoRivalBooking {
     function clearRegistration() external {
          require(registeredUsers[msg.sender], "you're not currently registered");
          registeredUsers[msg.sender] = false;
+         --totalRegistered;
          emit RegistrationCleared(true);
     }
 
     function bookRoom(uint8 hour, uint8 roomNum) external {
         require(registeredUsers[msg.sender], "you have to register before booking room");
+        require(roomNum <= roomLimit && roomNum >= 1, "room doesn't exist");
         string memory _group = userToGroup[msg.sender];
         if(keccak256(bytes(_group)) == keccak256(bytes(groupA))){
-            uint8[24] memory h = groupABooking[msg.sender][roomNum];
+            uint8[24] memory h = groupABooking[roomNum][msg.sender];
             require(h[hour] == 0, "you already booked this hour");
             h[hour] += 1;
-            groupABooking[msg.sender][roomNum] = h;
+            groupABooking[roomNum][msg.sender] = h;
         }else{
-            uint8[24] memory h = groupBBooking[msg.sender][roomNum];
+            uint8[24] memory h = groupBBooking[roomNum][msg.sender];
             require(h[hour] == 0, "you already booked this hour");
             h[hour] += 1;
-            groupBBooking[msg.sender][roomNum] = h;
+            groupBBooking[roomNum][msg.sender] = h;
         }
-        address user = msg.sender;
-        ActiveUsers.push(user);
+        registeredUsers[msg.sender] = true;
+        ++totalBooked;
         emit RoomBooked(roomNum, hour);
     }
 
-    function checkIfAlreadyBooked(string memory _group, 
-    uint8 hour, uint8 roomNum, address addr) internal view returns(bool){
+    function getRoom(uint8 roomNum, string calldata _group) external view returns(uint8[24] memory){
+
+        uint8[24] memory _hours_out;
+
+        for(uint8 i=0; i < ActiveUsers.length;) {      
+            address addr = ActiveUsers[i];          
+            if(registeredUsers[addr]){
+
+                   uint8[24] memory _hours;
+                   if(keccak256(bytes(_group)) == keccak256(bytes(groupA))){
+                       _hours = groupABooking[roomNum][addr];
+                   }else{
+                       _hours = groupBBooking[roomNum][addr];
+                   }
+                   for(uint8 z = 0;z< 24;z++){
+                        _hours_out[z] += _hours[z];
+                   }
+
+            }
+            i++;
+        }
+        return _hours_out;
+    }
+
+    function checkIfAlreadyBooked(string memory _group, uint8 hour, uint8 roomNum, address addr) internal view returns(bool){
         bool result = false;
         if(keccak256(bytes(_group)) == keccak256(bytes(groupA))){
-            uint8[24] memory h = groupABooking[addr][roomNum];
+            uint8[24] memory h = groupABooking[roomNum][addr];
             if(h[hour] < 1){ result = true; }
         }else{
-            uint8[24] memory h = groupBBooking[addr][roomNum];
+            uint8[24] memory h = groupBBooking[roomNum][addr];
             if(h[hour] > 0){ result = true; }
         }
         return result;
     }
 
     function totalBookings() external view returns(uint256){
-        return ActiveUsers.length;
-    }
-
-    function getRoom(uint8 room) external view returns(uint8[24] memory){
-        
-        string memory _group = userToGroup[msg.sender];
-        uint8[24] memory roomsBooked;
-
-        for(uint8 i=0; i < ActiveUsers.length; i++) {      
-            address addr = ActiveUsers[i];
-            if(keccak256(bytes(_group)) == keccak256(bytes(groupA))){
-                roomsBooked = groupABooking[addr][room];
-            }else{
-                roomsBooked = groupBBooking[addr][room];
-            }
-        }
-        return roomsBooked;
+        return totalBooked;
     }
 
 }
--- test/index.ts
+++ test/index.ts
@@ -13,43 +13,44 @@ describe("TwoRivalBooking", function () {
     expect(contract.address);
     console.log("deployed to: " + contract.address);
   });
-  it("Register User", async function () {
+  it("Register User GROUP P", async function () {
     assert(await contract.registerUser("P"));
   });
   it("Check User Registered", async function () {
     expect(await contract.registrationStatus()).to.equal(true);
   });
-  it("Total Registered", async function () {
-    assert(await contract.totalRegistered());
-    const bigNum = await contract.totalRegistered();
-    console.log("registered: " + bigNum.toNumber());
-  });
-  it("Book Room", async function () {
+  it("Book Rooms", async function () {
     assert(await contract.bookRoom(1, 1));
-    assert(await contract.bookRoom(0, 1));
     assert(await contract.bookRoom(2, 1));
+    // assert(await contract.bookRoom(2, 1));
+    assert(await contract.bookRoom(3, 1));
+    assert(await contract.bookRoom(1, 2));
+    assert(await contract.bookRoom(2, 3));
+    assert(await contract.bookRoom(3, 3));
+    assert(await contract.bookRoom(4, 3));
+  });
+  it("Fail on book same room and hour", async function () {
     await expect(contract.bookRoom(2, 1)).to.be.revertedWith(
       "you already booked this hour"
     );
-    assert(await contract.bookRoom(2, 2));
-    assert(await contract.bookRoom(23, 3));
   });
-  it("Total Users Booked", async function () {
-    expect(await contract.totalBookings()).to.equal(5);
+  it("", async function () {
+    console.log("\n-- ROOM COUNT -------------");
+    // just test with first 3 rooms
+    console.log(await contract.getRoom(1, "P"));
+    console.log(await contract.getRoom(2, "P"));
+    console.log(await contract.getRoom(3, "P"));
+    console.log("-----------------------------");
   });
-  it("Unregister", async function () {
+  it("Un Register", async function () {
     assert(await contract.clearRegistration());
   });
-  it("Get Rooms", async function () {
-    console.log(await contract.getRoom(1));
-    console.log(await contract.getRoom(2));
-    console.log(await contract.getRoom(3));
-    console.log(await contract.getRoom(4));
-    console.log(await contract.getRoom(5));
-    console.log(await contract.getRoom(6));
-    console.log(await contract.getRoom(7));
-    console.log(await contract.getRoom(8));
-    console.log(await contract.getRoom(9));
-    console.log(await contract.getRoom(10));
+  it("", async function () {
+    console.log("\n-- ROOM COUNT -------------");
+    // just test with first 3 rooms
+    console.log(await contract.getRoom(1, "P"));
+    console.log(await contract.getRoom(2, "P"));
+    console.log(await contract.getRoom(3, "P"));
+    console.log("-----------------------------");
   });
 });
